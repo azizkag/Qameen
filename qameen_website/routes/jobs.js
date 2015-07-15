4651cd83e3949d312677c72951c163555e1f1c4e
Aziz Alghunaim <azizkag@gmail.com>
2015-07-15 05:36:02 +0300

    retrieve jobs from the server

--- qameen_website/routes/jobs.js
+++ qameen_website/routes/jobs.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var Parse = require('parse').Parse;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('jobs', { title: 'Express Aziz' });
+Parse.initialize("ZYXlOAmL91AMn8kiog3kwA00r1cQB0HACIWUz75Y", "wXN6wTPTZ5DHesOyDYYQlwg8bJyvyRBXxSHFkt0R");
+
+
+var Jobs = Parse.Object.extend("Job");
+var query = new Parse.Query(Jobs);
+query.equalTo("status", 0);
+query.find({
+  success: function(results) {
+    console.log("Successfully retrieved " + results.length + " jobs.");
+    // Do something with the returned Parse.Object values
+    for (var i = 0; i < results.length; i++) {
+      var object = results[i];
+      console.log(object.id + ' - ' + object.get('company'));
+    }
+    //if objects retrived pass them to the page
+    /* GET home page. */
+	router.get('/', function(req, res, next) {
+  		res.render('jobs', { jobs: results });
+	});
+  },
+  error: function(error) {
+    console.log("Error: " + error.code + " " + error.message);
+    //if there was a problem, pass the error to the page
+    router.get('/', function(req, res, next) {
+  		res.render('jobs', { err: error });
+	});
+  }
 });
+
+
+
+
 
 
 module.exports = router;
