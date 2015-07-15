4402b0fd0750ea9c663eed9b350ca52cd6346d9f
Aziz Alghunaim <azizkag@gmail.com>
2015-07-15 04:34:17 +0300

    added the html template

--- qameen_website/app.js
+++ qameen_website/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var jobs = require('./routes/jobs');
+
 
 var app = express();
 
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/jobs', jobs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
